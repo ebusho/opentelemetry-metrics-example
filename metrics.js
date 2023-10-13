f7bae4e6290bab1cd7b7e47fbd550ede642bbce1
Alis Adrović <alis.adrovic@gmail.com>
2023-10-13 12:35:54 +0200

    Add InfluxDB exporter with routing processor

--- metrics.js
+++ metrics.js
@@ -3,7 +3,11 @@
 const { metrics, diag, DiagConsoleLogger, DiagLogLevel } = require("@opentelemetry/api");
 const { MeterProvider, PeriodicExportingMetricReader } = require("@opentelemetry/sdk-metrics");
 
-const { OTLPMetricExporter } = require("@opentelemetry/exporter-metrics-otlp-grpc");
+const { SemanticResourceAttributes } = require('@opentelemetry/semantic-conventions');
+
+const { OTLPMetricExporter } = require('@opentelemetry/exporter-metrics-otlp-grpc'); // port 4317
+// const { OTLPMetricExporter } = require("@opentelemetry/exporter-metrics-otlp-http"); // port 4318
+// const { OTLPMetricExporter } = require("@opentelemetry/exporter-metrics-otlp-proto"); // port 4318
 
 const { Resource } = require("@opentelemetry/resources");
 
@@ -15,11 +19,16 @@ diag.setLogger(new DiagConsoleLogger(), {
 
 // Object that holds all the metrics we create
 const meterProvider = new MeterProvider({
-  resource: new Resource({ "service.name": "opentelemetry-metrics-example" })
+  resource: new Resource({ [SemanticResourceAttributes.SERVICE_NAME]: "opentelemetry-metrics-example" })
 });
 
 // Object that sends metrics to OTel collector
-const metricExporter = new OTLPMetricExporter();
+const metricExporter = new OTLPMetricExporter({
+  // url: "http://localhost:4318/v1/metrics",
+  url: "http://localhost:4317"
+  // url: "http://opentelemetry-collector.dappradar-observability.svc.cluster.local:4318/v1/metrics",
+  // url: "http://opentelemetry-collector.dappradar-observability.svc.cluster.local:4317"
+});
 
 // Object that reads the accumulated metrics from the provider and sends them to the MetricsExporter
 const metricReader = new PeriodicExportingMetricReader({
